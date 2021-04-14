15289e228fd00654c8dfc482b862f9da5788312f
Ran Vaknin <ranvaknin0@gmail.com>
2021-04-14 16:46:56 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      flat
+      color="white"
+    >
+      <Header/>
+    </v-app-bar>
+
+    <v-content>
+      <Carousel/>
+    </v-content>
+
+    <v-main>
+
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header';
+import Carousel from './components/Carousel';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Header, Carousel
+  },
+
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-container class="carousel">
+    <div class="carousel_container">
+      <template>
+        <v-carousel
+            hide-delimiters
+            cycle
+            show-arrows-on-hover>
+          <v-carousel-item class="carousel_item"
+              v-for="(item,i) in items"
+              :key="i"
+              :src="item.src"
+              reverse-transition="fade-transition"
+              transition="fade-transition"
+          >
+            <v-row
+                v-bind:style="{backgroundColor:bgColor}"
+                @mouseover="hoverIn()"
+                @mouseout="hoverOut()"
+                class="fill-height"
+                align="center"
+                justify="center"
+            >
+              <div class="display-3">
+                <p class="carousel_title">
+                  {{ item.title }}
+                </p>
+              </div>
+            </v-row>
+          </v-carousel-item>
+        </v-carousel>
+      </template>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Carousel',
+  data: () => ({
+    bgColor: 'rgba(255,255,255,0.15)',
+      items: [
+        {
+          src: require('../assets/pearshape.png'),
+          title: 'Exquisite Cutting'
+        },
+        {
+          src: require('../assets/hex.png'),
+          title: 'One of a kind',
+        },
+        {
+          src: require('../assets/bluehex.png'),
+          title: 'Unbeatable Prices'
+        },
+        {
+          src: require('../assets/oval.png'),
+          title: 'Ethically Sourced'
+
+        },
+      ]
+  }),
+  methods: {
+    hoverIn() {
+      this.bgColor = 'rgba(255,255,255,0)'
+
+    },
+    hoverOut() {
+      this.bgColor = 'rgba(255,255,255,0.15)'
+    }
+  }
+}
+
+</script>
